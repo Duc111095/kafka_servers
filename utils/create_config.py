633b38a006799888058f466a07ce21269f4e58af
Duc111095 <huynhduc111095@gmail.com>
2024-11-04 16:32:06 +0700

    except zuliprc

--- utils/create_config.py
+++ utils/create_config.py
@@ -4,11 +4,11 @@ config = configparser.ConfigParser()
 
 with open('C:/Users/USER/PycharmProjects/kafka_servers/utils/server.txt', 'r') as f:
     for line in f.readlines():
-        option = line.split()
-        config[option[0]] = {'server': option[1],
-                                'database': option[2],
-                                'username': option[3],
-                                'password': option[4]}
+        options = line.split()
+        config[options[0]] = {'server': options[1],
+                                'database': options[2],
+                                'username': options[3],
+                                'password': options[4]}
 
 with open('config.ini', 'w') as configfile:
     config.write(configfile)
