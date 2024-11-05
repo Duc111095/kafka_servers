1c88ff63be73131df67c9025192b938df8b00961
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2024-11-05 15:34:17 +0700

    add features

--- main.py
+++
@@ -1,8 +0,0 @@
-from services import kafka_services
-from utils import connect
-
-if __name__ == "__main__":
-    # Init Connection of Multiples DB
-    server_pool = connect.connect_db()
-    # Kafka consumer
-    kafka_services.kafka_consumer(server_pool)
--- utils/create_config.py
+++ utils/create_config.py
@@ -5,11 +5,11 @@ config = configparser.ConfigParser()
 
 with open('server.txt', 'r') as f:
     for line in f.readlines():
-        options = line.split()
-        config[options[0]] = {'server': options[1],
-                            'database': options[2],
-                            'username': options[3],
-                            'password': base64.b64encode(options[4].encode("utf-8"))}
+        stroptions = line.split()
+        config[stroptions[0]] = {'server': stroptions[1],
+                            'database': stroptions[2],
+                            'username': stroptions[3],
+                            'password': base64.b64encode(stroptions[4].encode("utf-8"))}
 
 with open('server.ini', 'w') as configfile:
     config.write(configfile)
