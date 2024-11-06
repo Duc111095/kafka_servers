1670b4e3e4fcbc3ad6f3806e5c98e4b6f80a02f1
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2024-11-06 14:10:27 +0700

    fix bugs

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from utils import connect
 if __name__ == "__main__":
     # Init Connection of Multiples DB
     server_pool = connect.connect_db()
+    print(server_pool)
     # Kafka consumer
     kafka_services.kafka_consumer(server_pool)
